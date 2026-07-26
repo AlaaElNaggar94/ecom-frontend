c495527be6e0516559d6406f0277657b5a35ab55
AlaaElNaggar94 <alaaelnaggar359@gmail.com>
2026-07-26 17:07:18 +0200

    add navbar and core-module and install bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+
+    CoreModule,
     BrowserModule,
     AppRoutingModule
   ],
